38e45973d3126111a35345638b90e27d01fa02f2
nguyentruongan280704 <110025394+nguyentruongan280704@users.noreply.github.com>
2022-08-01 23:43:30 +0700

    Add files via upload

---
+++ bai tap ve nha 1-8/app.js
@@ -0,0 +1,69 @@
+// bai 1
+// for (let i = 1; i < 100; i = i + 1){
+//     console.log(i);
+// }
+    
+// bai 2
+// let a = 0 , b = 1 , nextNumber
+// for(i = 0 ; i <= 20 ; i = i + 1){
+//     console.log( b )
+//     nextNumber = a + b;
+//     a = b;
+//     b = nextNumber;
+// }
+
+
+
+// bai 5
+// let count = 0, tong =0
+//  let dayso =""
+//  for (let i=1; ; i++){
+//   if(i%7==0){
+//   dayso += i+" "
+//   console.log(dayso)
+//  tong += i
+//  count++
+// if(count==30) break
+//    }
+// }
+// document.write(dayso+"<br>")
+// document.write("Tổng  "+tong)
+    
+
+// bai 6
+// for (let i=1; i<=100; i=i+1){
+// if (i%3==0 && i%5 ==0){
+//  console.log(i+" FizzBuzz <br>")
+//  }else {
+//  if (i%3==0){
+//   console.log(i+" Fizz <br>")
+//  }
+//        else if(i%5==0){
+//             console.log(i+" Buzz <br>")
+//      }
+//     }
+// }
+
+//     // bai 4 
+// let F = 0 , F0= 0, F1=0, tong=0
+// let fibonacci = "";
+// for (let i=0; i<20;i++){
+//     if(i==0){
+//         F=1
+//         F1=1
+//         fibonacci += F+ " "
+//         tong +=F
+//     }else{
+//         F= F0+F1
+//         F0 = F1
+//         F1 = F
+//         fibonacci += F+ " "
+//         tong +=F
+//         console.log(i +" = "+F)
+//     }
+// }
+// console.log("Dãy Fibonacci là "+fibonacci+"<br>")
+// console.log("Tổng của các số = "+tong)
+
+
+// em xin lỗi thầy e không làm được ạ :((
---
+++ baitaptrenlop/appbttl.js
@@ -0,0 +1,106 @@
+// bai1
+// let n = Number(prompt("nhap so"));
+// let tong = 0
+// for (let i = 1; i <= n; i = i + 1 ){
+//     tong = tong + i;
+//  }
+// console.log(`tong = ${tong}`);
+ 
+// bai2
+// let n = Number(prompt("nhap so "));
+// let tong = 1
+// for(let i = 1; i <= n; i = i + 1){
+//     tong = tong * i;
+// }
+//    console.log(`tong = ${tong}`);
+
+
+// bai 3 
+// let n = Number(prompt("nhap so"));
+// let tong = 0
+// for (let i = 1; i <= n; i = i + 1){
+//     tong =  tong + 1/i
+// }
+//   console.log(`tong = ${tong}`);
+
+
+// bai 4
+// let n = Number(prompt("nhap so"));
+// let tong = 0;
+// for(let i = 2; i <= n; i = i + 1){
+//     tong = tong + 1/(n-1) * 1/n;
+// }
+//   console.log(`${tong} ` );
+
+
+// bai 5 
+// let n = Number(prompt("nhap so"));
+// let tong = 1
+// for(let i = 1; i <= 10; i = i + 1){
+//    tong = n * i
+// console.log(`${n} * ${i} = ${tong}`);
+//
+
+// bai 7 
+// let n = Number(prompt("nhap so"))
+// for(let i = 1; i <= n; i = i + 1){
+//     if (i % 2 === 0){
+//         console.log(`${i} chan`);
+//     }else{
+//         console.log(`${i} le`);
+//     }
+// }
+
+
+
+// let a = 5
+// if (a % 5 === 0 ){
+//    alert("dung");
+// }else{
+//     alert("sai");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let n = Number(prompt("nhap"));
+// for(i = 1; i <= n; i = i + 1){
+//     if (i % 2 === 0) {
+//         console.log(`${i} chan`);
+//     }else{
+//         console.log(`${i} le`);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
